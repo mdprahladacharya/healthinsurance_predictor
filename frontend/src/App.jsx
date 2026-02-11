2038cfc53b9215985a4032c80b6105780f44a7a6
anushree-hemadri123 <anushreehemadri30@gmail.com>
2026-02-11 23:25:23 +0530

    Frontend UI with Tailwind + chart integration, ML prediction working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,9 @@ function App() {
     <div>
       <h1>Insurance Predictor</h1>
       <PredictForm />
+      <h1 className="text-5xl text-red-500 text-center">
+      TAILWIND TEST
+      </h1>
     </div>
   );
 }
--- frontend/src/components/PredictForn.jsx
+++ frontend/src/components/PredictForn.jsx
@@ -18,38 +18,64 @@ function PredictForm() {
   };
 
   const handlePredict = async () => {
+    if (!form.age || !form.bmi || !form.children) {
+      alert("Fill all fields");
+      return;
+    }
+
     const res = await axios.post("http://127.0.0.1:8000/predict", form);
     setResult(res.data.predicted_cost);
   };
 
   return (
-    <div>
-      <h2>Enter Details</h2>
+    <div className="min-h-screen bg-gradient-to-r from-purple-500 to-blue-500 flex items-center justify-center">
+      
+      <div className="bg-white/20 backdrop-blur-lg p-8 rounded-2xl shadow-xl w-[400px] text-white">
+        
+        <h1 className="text-3xl font-bold mb-6 text-center">
+          💊 Insurance Predictor
+        </h1>
 
-      <input name="age" placeholder="Age" onChange={handleChange} />
-      <input name="bmi" placeholder="BMI" onChange={handleChange} />
-      <input name="children" placeholder="Children" onChange={handleChange} />
+        <input className="w-full p-2 mb-3 rounded text-black"
+          name="age" placeholder="Age" onChange={handleChange}/>
 
-      <select name="sex" onChange={handleChange}>
-        <option value="male">Male</option>
-        <option value="female">Female</option>
-      </select>
+        <input className="w-full p-2 mb-3 rounded text-black"
+          name="bmi" placeholder="BMI" onChange={handleChange}/>
 
-      <select name="smoker" onChange={handleChange}>
-        <option value="no">No</option>
-        <option value="yes">Yes</option>
-      </select>
+        <input className="w-full p-2 mb-3 rounded text-black"
+          name="children" placeholder="Children" onChange={handleChange}/>
 
-      <select name="region" onChange={handleChange}>
-        <option value="northwest">Northwest</option>
-        <option value="northeast">Northeast</option>
-        <option value="southwest">Southwest</option>
-        <option value="southeast">Southeast</option>
-      </select>
+        <select className="w-full p-2 mb-3 rounded text-black" name="sex" onChange={handleChange}>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
 
-      <button onClick={handlePredict}>Predict</button>
+        <select className="w-full p-2 mb-3 rounded text-black" name="smoker" onChange={handleChange}>
+          <option value="no">Non-Smoker</option>
+          <option value="yes">Smoker</option>
+        </select>
 
-      {result && <h2>Predicted Cost: {result}</h2>}
+        <select className="w-full p-2 mb-3 rounded text-black" name="region" onChange={handleChange}>
+          <option value="northwest">Northwest</option>
+          <option value="northeast">Northeast</option>
+          <option value="southwest">Southwest</option>
+          <option value="southeast">Southeast</option>
+        </select>
+
+        <button
+          onClick={handlePredict}
+          className="w-full bg-blue-600 hover:bg-blue-700 p-3 rounded-lg mt-2"
+        >
+          Predict Cost
+        </button>
+
+        {result && (
+          <div className="mt-5 bg-white/30 p-4 rounded-lg text-center">
+            <h3>Estimated Cost</h3>
+            <h2 className="text-2xl font-bold">₹ {result}</h2>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
